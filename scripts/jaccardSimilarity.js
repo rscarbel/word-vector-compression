e336af459bdf9dcf7a6bb7ea4525fd06e971ae31
Ryan Scarbel <40727301+rscarbel@users.noreply.github.com>
2023-05-07 14:40:38 -0400

    fixed jaccard similarity

--- scripts/jaccardSimilarity.js
+++ scripts/jaccardSimilarity.js
@@ -1,9 +1,9 @@
 const jaccardSimilarity = (str1, str2) => {
-  const str1Arr = str1.split(" ");
-  const str2Arr = str2.split(" ");
-  const intersection = str1Arr.filter((word) => str2Arr.includes(word));
-  const union = [...str1Arr, ...str2Arr];
-  return intersection.length / union.length;
+  const str1Arr = new Set(str1.split(" "));
+  const str2Arr = new Set(str2.split(" "));
+  const intersection = [...str1Arr].filter((word) => str2Arr.has(word));
+  const union = new Set([...str1Arr, ...str2Arr]);
+  return intersection.length / union.size;
 };
 
 module.exports = jaccardSimilarity;
