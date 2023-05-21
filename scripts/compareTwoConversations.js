8a5d13718c902f60c90b07d0229c1d8ac532332b
Ryan Scarbel <40727301+rscarbel@users.noreply.github.com>
2023-05-21 13:49:02 -0400

    removed now unused method

--- scripts/compareTwoConversations.js
+++ scripts/compareTwoConversations.js
@@ -2,7 +2,6 @@ const convertToMatrix = require("./convertToMatrix");
 const calculateMeanPosition = require("./operations_using_matrices/calculateMeanPosition");
 const vectorCosineSimilarity = require("./operations_using_matrices/vectorCosineSimilarity");
 const euclideanDistance = require("./operations_using_matrices/euclideanDistance");
-const dbscan = require("./operations_using_matrices/DBSCAN/dbscan");
 const hausdorffDistance = require("./operations_using_matrices/hausdorffDistance");
 const { error } = require("console");
 
