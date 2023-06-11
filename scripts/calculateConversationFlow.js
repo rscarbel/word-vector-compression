02e1a85cee49a04dd2d38079d27de43d4449b466
Ryan Scarbel <40727301+rscarbel@users.noreply.github.com>
2023-06-11 17:46:02 -0400

    improved algorithm time

--- scripts/calculateConversationFlow.js
+++ scripts/calculateConversationFlow.js
@@ -1,23 +1,24 @@
 const matrixWithWords = require("./matrixWithWords");
 const dbscanWithAllData = require("./operations_using_matrices/DBSCAN/dbscanWithAllData");
 const inflection = require("inflection");
-const { default: cluster } = require("cluster");
 
-const MINIMUM_DISTANCE = 3;
-const epsilon = 4;
+const epsilon = 3;
 
 const calculateConversationFlow = async (conversation) => {
   lowerCaseConversation = conversation.toLowerCase();
+  const timeBeforeLookup = new Date().getTime();
   const matrix = await matrixWithWords(lowerCaseConversation);
+  const timeAfterLookup = new Date().getTime();
+  const lookupTimeElapsed = timeAfterLookup - timeBeforeLookup;
+  const lookupTimeInSeconds = lookupTimeElapsed / 1000;
 
   const minPts = Math.ceil(matrix.length * 0.012) + 1;
 
-  const currentTime = new Date().getTime();
+  const timeBeforeDBSCAN = new Date().getTime();
   const dbscanResult = await dbscanWithAllData(matrix, epsilon, minPts);
-  const newTime = new Date().getTime();
-  const timeElapsed = newTime - currentTime;
-  const timeInSeconds = timeElapsed / 1000;
-  console.log("DBSCAN runtime (seconds):", timeInSeconds);
+  const timeAfterDBSCAN = new Date().getTime();
+  const timeElapsed = timeAfterDBSCAN - timeBeforeDBSCAN;
+  const DBSCANTime = timeElapsed / 1000;
   const flattenedClusterWords = dbscanResult.flattenedClusterWords || [];
 
   const sentences = lowerCaseConversation.split(/(?<=[.!?])\s+(?=\D|$)/);
@@ -62,10 +63,10 @@ const calculateConversationFlow = async (conversation) => {
     }
 
     for (let i = 0; i < sentenceComposition.length; i++) {
-      const roundedPercentage = Math.round(
-        (sentenceComposition[i].count / totalTopicCount) * 100
-      );
-      sentenceComposition[i].percentage = `${roundedPercentage}%`;
+      const roundedPercentage = (
+        sentenceComposition[i].count / totalTopicCount
+      ).toFixed(2);
+      sentenceComposition[i].percentage = roundedPercentage;
     }
 
     sentenceMembership.push(sentenceComposition);
@@ -73,15 +74,17 @@ const calculateConversationFlow = async (conversation) => {
 
   prettifiedSentenceMembership = sentenceMembership
     .map((sentence) => {
-      return sentence
+      return `[&nbsp;${sentence
         .map((word) => {
-          return `${word.word} (${word.percentage})`;
+          return `${word.word}: ${word.percentage}`;
         })
-        .join(", ");
+        .join(", ")}&nbsp;] `;
     })
     .join("<br>");
 
   const message = `
+  <strong>Database Lookup Time:</strong> ${lookupTimeInSeconds} seconds<br>
+  <strong>Algorithm Time:</strong> ${DBSCANTime} seconds<br>
   <strong>Topics:</strong><br>${flattenedClusterWords.join(", ")}
   <br>---------------------<br>
   <strong>Sentence Membership:</strong><br>${prettifiedSentenceMembership}`;
